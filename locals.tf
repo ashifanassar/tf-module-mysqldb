0b62903df6c1730b1c4b39f61a8e9540502de28f
ashifanassar <ashifanassar@gmail.com>
2024-05-01 04:05:58 +0530

    module

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  RDS_USERNAME = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["RDS_USERNAME"]  
-  RDS_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["RDS_PASSWORD"]
+  RDS_USERNAME = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_USERNAME"]  
+  RDS_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_PASSWORD"]
 }
