dec3bc763437d55deb135b11b42e2f578698a1dd
ashifanassar <ashifanassar@gmail.com>
2024-05-01 03:47:55 +0530

    module

--- data.tf
+++ data.tf
@@ -7,3 +7,16 @@ data "terraform_remote_state" "vpc" {
     region = "us-east-1"
   }
 }
+
+#secret manager extraction
+
+data "aws_secretsmanager_secret" "secrets" {
+  name = "robot/secrets"
+}
+
+
+
+#To retrive the secret maanger version with secret
+data "aws_secretsmanager_secret_version" "secret_version" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
