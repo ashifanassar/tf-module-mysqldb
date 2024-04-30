779b8b06bf63c76fee46a474ec4f8ba29fde8b6d
ashifanassar <ashifanassar@gmail.com>
2024-04-30 20:05:32 +0530

    modules

--- mysql.tf
+++ mysql.tf
@@ -1,14 +1,14 @@
 # Provisions MySQL RDS Instance 
 resource "aws_db_instance" "mysql" {
-  allocated_storage        = 10
+   allocated_storage        = var.MYSQL_STORAGE
   identifier               = "roboshop-${var.ENV}-mysql"
-  engine                   = "mysql"
-  engine_version           = "5.7"
-  instance_class           = "db.t3.micro"
+  engine                   = var.MYSQL_ENGINE
+  engine_version           = var.MYSQL_ENGINE_VERSION
+  instance_class           = var.MYSQL_INSTANCE_TYPE
   username                 = "admin1"
   password                 = "RoboShop1"
   parameter_group_name     = aws_db_parameter_group.mysql.name 
-  skip_final_snapshot      = true 
+  skip_final_snapshot      = var.MYSQL_SKIP_SNAPSHOT
   db_subnet_group_name     = aws_db_subnet_group.mysql.name 
   vpc_security_group_ids   = [aws_security_group.allow_mysql.id]
 }
@@ -16,7 +16,7 @@ resource "aws_db_instance" "mysql" {
 # Provisons Parameter Groups for RDS
 resource "aws_db_parameter_group" "mysql" {
   name                = "rds-pg"
-  family              = "mysql5.7"
+  family              = var.MYSQL_ENGINE_FAMILY
 }
 
 # Provisions Subnet Group
--- vars.tf
+++ vars.tf
@@ -1,1 +1,8 @@
 variable "ENV" {}
+variable "MYSQL_STORAGE" {}
+variable "MYSQL_ENGINE" {}
+variable "MYSQL_ENGINE_VERSION" {}
+variable "MYSQL_INSTANCE_TYPE" {}
+variable "MYSQL_SKIP_SNAPSHOT" {}
+variable "MYSQL_PORT" {}
+variable "MYSQL_ENGINE_FAMILY" {}
