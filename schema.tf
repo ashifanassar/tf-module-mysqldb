8a98a299613d543d468ed83b7fa5a44373c0a2b9
ashifanassar <ashifanassar@gmail.com>
2024-05-01 05:30:35 +0530

    module

---
+++ schema.tf
@@ -0,0 +1,15 @@
+resource "null_resource" "schema" {
+
+    # This make sures that this null_resource will only be executed post the creation of the RDS only    
+    depends_on = [aws_db_instance.mysql]
+
+      provisioner "local-exec" {
+        command = <<EOF
+            cd /tmp
+            curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+            unzip -o mysql.zip
+            cd mysql-main
+            mysql -h ${aws_db_instance.mysql.address}  -uadmin1 -pRoboShop1 < shipping.sql
+        EOF
+    }
+}
