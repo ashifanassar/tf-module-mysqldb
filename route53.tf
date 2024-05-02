9a6e5a47fc15d25702adc2882d1e549ddd290da9
ashifanassar <ashifanassar@gmail.com>
2024-05-02 19:25:33 +0530

    module

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "mysql" {
-  zone_id = "Z0547409165EGAKUG3EH3"
-  name    = "mysql-${var.ENV}.roboshopshopping"
+  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID
+  name    = "mysql-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_NAME}"
   type    = "CNAME"
   ttl     = 10
   records = [aws_db_instance.mysql.address]
